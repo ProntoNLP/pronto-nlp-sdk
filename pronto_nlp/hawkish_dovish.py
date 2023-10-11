18e31f8f6a35c3cda1a54e0bc7d84031564eb2e6
ilayofirdooble <124139625+ilayofirdooble@users.noreply.github.com>
2023-10-11 15:58:32 +0300

    test 1

--- pronto_nlp/hawkish_dovish.py
+++ pronto_nlp/hawkish_dovish.py
@@ -5,124 +5,32 @@ import boto3
 import websocket
 import argparse
 import csv
+import os
+from . import ProcessingAPI_MacroLLM
+# from .ProcessingAPI_MacroLLM import SignIn, ProcessSentences
 
+# def process_document(args):
+#     authToken = SignIn(args['user'], args['password'])
+#     print("Authentication successful")
 
-def SignIn(user, password):
-    requestObj = {"email": user, "password": password, "organization": "dev"}
-    body = json.dumps(requestObj, ensure_ascii=True).encode('ascii')
-    request = urllib.request.Request("https://server-dev.prontonlp.com/token", data=body, headers={"Content-Type": "application/json"})
+#     with open(args['input'], 'rt', encoding='utf-8', errors="ignore") as F:
+#         sentences = F.readlines()
+#     sentences = [s.strip() for s in sentences if s.strip()]
 
-    try:
-        response = urllib.request.urlopen(request)
-        if response.status == 200:
-            result = response.read().decode('utf-8')
-            if not result.startswith('{'): return result
-    except:
-        pass
+#     def ProgressReport(iAt):
+#         print(f"Processed: {iAt} of {len(sentences)} ({iAt*100.0/len(sentences):.4f}%)", end='\r', file=sys.stderr)
 
-    authParameters = {'USERNAME': user, 'PASSWORD': password}
-    client = boto3.client('cognito-idp', region_name='us-west-2')
-    response = client.initiate_auth(AuthFlow='USER_PASSWORD_AUTH', AuthParameters=authParameters, ClientId='88nt7a64nkbukf7r49p8q82ht')
-    authToken = response['AuthenticationResult']['IdToken']
-    return authToken
+#     allResults = ProcessSentences(authToken, sentences, ProgressReport, args['maxparallel'])
+#     print(file=sys.stderr)
 
+#     with open(args['output'], "w", encoding="utf-8", errors="ignore") as FOut:
+#         CSVWriter = csv.writer(FOut, lineterminator='\n')
+#         CSVWriter.writerow(["sentence", "result"])
+#         for sentence, result in zip(sentences, allResults):
+#             CSVWriter.writerow([sentence, json.dumps(result)])
 
-def ProcessDoc_websocketAPI(authToken, ruleset, outputtype, text):
-    ws = websocket.create_connection("wss://3c5qlj48hi.execute-api.us-west-2.amazonaws.com/main")
-
-    try:
-        iAt = 0
-        iIndex = 0
-        while iAt < len(text) - 20000:
-            #print(f"Storing {iIndex} {iAt}", file=sys.stderr)
-            ws.send(json.dumps({'request': 'WSProcessDoc-StoreData', 'authtoken': authToken,
-                                'index': iIndex, 'data': text[iAt:iAt+20000]}))
-            ack = ws.recv()
-            #print(f"Storing-ack {ack}", file=sys.stderr)
-            if json.loads(ack)['Collected'] != iIndex: raise Exception("Incorrect store-data acknowledgement")
-            iAt += 20000
-            iIndex += 1
-
-        #print(f"WSProcessDoc", file=sys.stderr)
-        ws.send(json.dumps({'request': 'WSProcessDoc', 'authtoken': authToken,
-                            'ruleset': ruleset, 'outputtype': outputtype, 'text': text[iAt:]}))
-
-        #print(f"Receiving", file=sys.stderr)
-        iSize = int(ws.recv())
-        #print(f"Received size {iSize}", file=sys.stderr)
-        result = ""
-        while len(result) < iSize:
-            #print(f"Receiving next", file=sys.stderr)
-            result += json.loads(ws.recv())
-
-        return result
-
-    finally:
-        ws.close()
-
-
-def ReadMessageFromWebsocket(ws):
-    body = ws.recv()
-    if not body:
-        print("\nWarning: empty message - ignoring", file=sys.stderr)
-        return None
-    try:
-        return json.loads(body)
-    except:
-        print(f"\nError: JSON failed to decode message: '{body}'", file=sys.stderr)
-        raise
-
-
-def ProcessSentences(authToken, sentences, progressReportCallback=None, iMaxParallel=8):
-    results = [None for _ in sentences]
-    iCountResults = 0
-    ws = websocket.create_connection("wss://cxyh53vkmj.execute-api.us-west-2.amazonaws.com/main")
-    try:
-        iAt = 0
-        iInFlight = 0
-        while True:
-            while iInFlight < iMaxParallel and iAt < len(sentences):
-                ws.send(json.dumps({'authtoken': authToken, 'request': 'MacroLLM_ProcessSentence_Async', 'index': iAt, 'sentence': sentences[iAt]}))
-                iAt += 1
-                iInFlight += 1
-            if iInFlight == 0: break
-            message = ReadMessageFromWebsocket(ws)
-            if not message: continue
-            if isinstance(message, str):
-                print(message, file=sys.stderr)
-                continue
-            #print(message, file=sys.stderr)
-            index, data = message['index'], message['data']
-            if results[index] is not None:
-                print("\nWarning: repeated result - ignoring", file=sys.stderr)
-                continue
-            results[index] = data
-            iCountResults += 1
-            iInFlight -= 1
-            if progressReportCallback: progressReportCallback(iCountResults)
-        return results
-    finally:
-        ws.close()
-
-def run_inference(args):
-    authToken = SignIn(args['user'], args['password'])
-    print("Authentication successful")
-
-    with open(args['input'], 'rt', encoding='utf-8', errors="ignore") as F:
-        sentences = F.readlines()
-    sentences = [s.strip() for s in sentences if s.strip()]
-
-    def ProgressReport(iAt):
-        print(f"Processed: {iAt} of {len(sentences)} ({iAt*100.0/len(sentences):.4f}%)", end='\r', file=sys.stderr)
-
-    allResults = ProcessSentences(authToken, sentences, ProgressReport, args['maxparallel'])
-    print(file=sys.stderr)
-
-    with open(args['output'], "w", encoding="utf-8", errors="ignore") as FOut:
-        CSVWriter = csv.writer(FOut, lineterminator='\n')
-        CSVWriter.writerow(["sentence", "result"])
-        for sentence, result in zip(sentences, allResults):
-            CSVWriter.writerow([sentence, json.dumps(result)])
+def process_document(input: str, output: str, user: str, password: str, maxparallel=8):
+    os.system('python3 ./hawkish_dovish.py  -u ' + user + ' -p ' + password + ' -i ' + input + ' -o ' + output + ' -n ' + str(maxparallel))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process document using Macro LLM.')
@@ -133,4 +41,6 @@ if __name__ == '__main__':
     parser.add_argument('-n', '--maxparallel', type=int, help='Maximal number of sentences processed in parallel', default=8)
     args = vars(parser.parse_args())
 
-    run_inference(args)
+    process_document(args['input'], args['output'], args['user'], args['password'], args['maxparallel'])
+
+    # python pronto_nlp/hawkish_dovish.py -u ... -p ... -i _DocForMacroTest.TXT -o RESULTS.CSV
