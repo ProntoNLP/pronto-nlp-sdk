a59a6783358b7626ad5bc4a6f0798ca1be82d7cf
Ilan Attar <53903741+iattar@users.noreply.github.com>
2023-10-30 13:55:15 +0200

    Update macro_test.py

--- tests/macro_test.py
+++ tests/macro_test.py
@@ -3,7 +3,7 @@ from pronto_nlp import macro
 success = macro.process_document(
     input="tests/_DocForMacroTest.txt",
     output="output.csv",
-    user="ilay@dooble.co.il",
-    password="Ilay6969"
+    user=org:user",
+    password="password"
 )
 print(success)
