b3cccfa0c264e47b6da19218722a61bc04691f1e
iattar <ilan.attar@gmail.com>
2024-05-15 16:41:02 +0300

    v3

--- tests/macro_test.py
+++ tests/macro_test.py
@@ -2,7 +2,7 @@ from pronto_nlp import macro
 
 print('Processing document')
 allResults = macro.process_document(
-    input="../samples/_DocForMacroTest.txt",
+    input="../samples/MacroTest.txt",
     output="output.csv",
     user="user@example.com",
     password="password"
